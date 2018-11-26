e0824fed7235d2ffb1b8fb6e940a5684816b5698
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-26 21:12:24 +0200

    render stories title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
+import fetch from "isomorphic-fetch";
 import React, { Component } from "react";
+import Error from "next/error";
 
 export default class Index extends Component {
+	static async getInitialProps() {
+		let stories;
+		try {
+			const response = await fetch(
+				"https://node-hnapi.herokuapp.com/news?pae=1"
+			);
+			stories = await response.json();
+		} catch (err) {
+			console.log(err);
+			stories = [];
+		}
+		return { stories };
+	}
 	render() {
-		return <div>hacker next</div>;
+		const { stories } = this.props;
+		if (stories.length === 0) {
+			return <Error statusCode={503} />;
+		}
+		return (
+			<div>
+				<h1>hacker next</h1>
+				<div>
+					{stories.map(story => (
+						<h2 key={story.id}>{story.title}</h2>
+					))}
+				</div>
+			</div>
+		);
 	}
 }
