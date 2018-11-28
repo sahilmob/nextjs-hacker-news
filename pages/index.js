ae4dc0110b46dd9cdf11025459820659b52c5dde
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-28 23:22:53 +0200

    add pagenation

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
 import fetch from "isomorphic-fetch";
 import React, { Component } from "react";
 import Error from "next/error";
+import Link from "next/link";
 
 import Layout from "../components/Layout";
 import StoryList from "../components/StoryList";
 
 export default class Index extends Component {
-	static async getInitialProps() {
+	static async getInitialProps({ req, res, query }) {
 		let stories;
+		let page;
 		try {
+			page = Number(query.page) || 1;
 			const response = await fetch(
-				"https://node-hnapi.herokuapp.com/news?pae=1"
+				`https://node-hnapi.herokuapp.com/news?page=${page}`
 			);
 			stories = await response.json();
 		} catch (err) {
 			console.log(err);
 			stories = [];
 		}
-		return { stories };
+		return { stories, page };
 	}
 	render() {
-		const { stories } = this.props;
+		const { stories, page } = this.props;
 		if (stories.length === 0) {
 			return <Error statusCode={503} />;
 		}
@@ -30,6 +33,23 @@ export default class Index extends Component {
 				description="A Hacker News clone made with Next.js"
 			>
 				<StoryList stories={stories} />
+				<footer>
+					<Link href={`/?page=${page + 1}`}>
+						<a>Next Page ({page + 1})</a>
+					</Link>
+				</footer>
+				<style jsx>
+					{`
+						footer {
+							padding: 1em;
+						}
+						footer a {
+							font-weight: bold;
+							color: black;
+							text-decoration: none;
+						}
+					`}
+				</style>
 			</Layout>
 		);
 	}
