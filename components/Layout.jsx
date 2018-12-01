7f24a208600374038308c71f37a7801959833238
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-01 11:43:25 +0200

    move styles to layout.csss

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Router from 'next/router'
 
+import "./layout.css";
+
 const Layout = ({ children, title, description, backButton }) => (
   <div>
     <Head>
@@ -21,36 +23,6 @@ const Layout = ({ children, title, description, backButton }) => (
       </nav>
       {children}
     </div>
-    <style jsx>
-      {
-        `
-        .container {
-          max-width: 800px;
-          margin: 0 auto;
-          background: #f6f6f6;
-        }
-        nav {
-          background: #f60;
-          padding: 1em;
-        }
-        nav > * {
-          display: inline-block;
-          color: black;
-        }
-        nav a {
-          text-decoration: none;
-        }
-        nav .main-title {
-          font-weight: bold;
-        }
-        nav .back-button {
-          font-size: 0.9rem;
-          padding-right: 1em;
-          cursor: pointer;
-        }
-        `
-      }
-    </style>
     <style global jsx>
       {
         `
